adc6e3bc8f9b844bc08b82aa08a05a9c75302cd7
Tadiwos Anegagregn <tadiwosanegagregn@gmail.com>
2024-06-10 22:21:31 +0300

    landing page first sight, uncleaned

---
+++ client/src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+import LandingPage from './pages/landingpage/landingPageLayout'
+import { BrowserRouter as Router } from 'react-router-dom'; 
+function App() {
+  return (
+    <>
+      <LandingPage />
+    </>
+  )
+}
+
+export default App
